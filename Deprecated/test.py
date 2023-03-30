1eb553f5898f162b0b0420fc6585bfb2ed15de7f
FurrySparkle <97181304+FurrySparkle@users.noreply.github.com>
2023-03-30 14:58:11 -0600

    NewsAPI and consistent GPT
    What a day, I have learned that python maps are dict objects and need to be convert to json for dot notation. So happy it all works 9/10 times

--- Deprecated/test.py
+++ Deprecated/test.py
@@ -3,3 +3,87 @@ print(sys.path)
 
 sys.path.append(C:\Python310\Lib\site-packages)
 print(sys.path)
+
+
+
+
+# -----------------------------------------------------------
+
+
+# app.py deprecate
+
+
+# Science Editor
+script = str(FinalScript)
+editorNewton = [
+		,contract
+		,editControl
+    ,{"role":"user", "content": script}]
+
+
+
+NewtonEdit = openai.ChatCompletion.create(
+
+		model = model,
+		messages = editorNewton,
+		max_tokens = 2300,
+		top_p=  0.25,
+		temperature= 0.18,
+		).choices[0].message.content
+
+
+
+
+
+# TechEditor Block
+script = str(NewtonEdit)
+editorLinusPenguin = [
+		,contract
+		,editControl
+    ,{"role":"user", "content": script}]
+
+
+
+PenguinEdit = openai.ChatCompletion.create(
+
+		model = model,
+		messages = editorLinusPenguin,
+		max_tokens = 2300,
+		top_p=  0.35,
+		temperature= 0.75,
+		).choices[0].message.content
+
+
+#Emotions Editor Block
+script = str(PenguinEdit)
+editorHaddaLamb = [		,contract
+    ,editControl
+  ,{"role":"user", "content": script}]
+
+
+LambEdit = openai.ChatCompletion.create(
+
+		model = model,
+		messages = editorHaddaLamb,
+		max_tokens = 2300,
+		top_p= 0.6 ,
+		temperature= 0.9,
+		).choices[0].message.content
+
+
+# This is an editor block, specifically notorious Culture Editor Lil Smoke
+script = str(LambEdit)
+editorLilSmoke = [		,contract
+		,editControl
+    ,{"role":"user", "content": script}]
+
+
+
+LilEdit = openai.ChatCompletion.create(
+
+		model = model,
+		messages = editorLilSmoke,
+		max_tokens = 2300,
+		top_p=  0.9,
+		temperature= 1.3,
+		)
